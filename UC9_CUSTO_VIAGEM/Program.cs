b295b27247858ab28568a6b5b5c78f33fb1007ac
FabricioAntonio21 <antoniofabricio016@gmail.com>
2023-03-01 21:52:54 -0300

    Criando Repositorio para o Custo_Viagem

---
+++ UC9_CUSTO_VIAGEM/Program.cs
@@ -0,0 +1,21 @@
+﻿// Criando Variavel
+Double valorabastecido, kmrodado, valorlitro, consumo;
+
+// Inicialiando as varivaveis
+valorabastecido = 0;
+kmrodado = 0;
+valorlitro = 0;
+consumo = 0;
+
+Console.WriteLine("Qual o valor pago pelo abastecimento?");
+valorabastecido = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantos Kms você percorreu com este abastecimento?");
+kmrodado = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Qual o valor do litro?");
+valorlitro = double.Parse(Console.ReadLine());
+
+consumo = kmrodado / (valorabastecido / valorlitro);
+
+Console.WriteLine("O consumo do seu veiculo é (KM / l): " + consumo);
